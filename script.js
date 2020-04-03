6e21924d0b4d8ddb198e6933b0cfaaf7a9f170ba
fruityvegetables <gg.troyer@gmail.com>
2020-04-03 12:50:41 -0500

    added some inputs returned

--- script.js
+++ script.js
@@ -13,14 +13,14 @@ $(document).ready(function() {
     var forecastQuery = "https://api.openweathermap.org/data/2.5/forecast?appid="+authKey+"&q=";
     var weatherQuery = "https://api.openweathermap.org/data/2.5/weather?appid="+authKey+"&q=";
     var uvQuery = "https://api.openweathermap.org/data/2.5/uvi?appid="+authKey;
-
-
+    
+    //fxns
     function kelvinToF(temp){
         return ((temp - 273.15)*(9/5) + 32).toFixed(2);
     }
 
-    function kelvinToC(temp){
-        return (temp - 273.15).toFixed(2);
+    function kelvinToC(temp2){
+        return (temp2 - 273.15).toFixed(2);
     }
 
     $(document).on("click","#searchButton", function(){
@@ -34,20 +34,49 @@ $(document).ready(function() {
 
              })
              .then(function(response){
-                var time = moment();
-                weatherEl.empty();
+                //var time = moment();
+                //weatherCard.empty();
+                weatherResults.empty();
+
                 var tempF = kelvinToF(response.main.temp);
-                $("#weatherEl").append("Temperature in Fahrenheit: " + tempF);
+                $("#weatherResults").append("Temperature in Fahrenheit: " + tempF + "   ");
                 var tempC = kelvinToC(response.main.temp);
-                $("#weatherEl").append("Temperature in Celsius: " + tempC);
+                $("#weatherResults").append("Temperature in Celsius: " + tempC + "   ");
+                var humidity = response.main.humidity;
+                $("#weatherResults").append("Humidity: " + humidity + "%   ");
+                var windSpeed = response.wind.speed;
+                $("#weatherResults").append("Wind Speed: " + windSpeed + "   mph");
+                $("#weatherResults").append($('<div/>',{
+                    id:"uvDiv",text:"UV Index: "
+                }));
                 
-                // var time = moment();
              })
+             
     })
-    userInput.on("submit", cityAdded);
-
-
-
-
-
+    getUV(response);
+    function getUV(response){
+        //get the UV index via ajax call
+        $.ajax({
+            url:uvQuery+"&lat="+response.coord.lat+"&lon="+response.coord.lon, 
+            method:"GET"
+        })
+        .then(function(uvresponse){
+            //console.log(uvresponse);
+            // scale index colors pulled from https://www.epa.gov/sunsafety/uv-index-scale-0
+            var uv = uvresponse.value;
+            var uvScale;
+    
+            //set class name based on severity of UV
+            if(uv < 3 ){uvScale = "uvLow"}
+            else if(uv < 6 ){uvScale = "uvMed"}
+            else if(uv < 8 ){uvScale = "uvHigh"}
+            else if(uv < 11 ){uvScale = "uvVeryHigh"}
+            else{uvScale = "uvExtreme"}
+            
+            //add index and appropriate class
+            $("#uvDiv").append($("<span/>",{
+                class:"uv "+uvScale, text:uv
+        }));
+        });
+    }
 });
